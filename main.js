b346d402029ca6e464fdc4676d63c27ba1ef07da
minji <lee.minji9521@gmail.com>
2024-03-29 16:07:05 -0400

    till render

--- main.js
+++ main.js
@@ -1,6 +1,34 @@
 const API_KEY = `f17c0665a4984e26955a5cdb01c37a40`
+let newsList = []
 
-const getLatestNews = () => {
-    const url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`
-    console.log("uuu", url);
+const getLatestNews = async() => {
+    let url = new URL(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`)
+    console.log("uuu", url)
+    const response = await fetch(url)
+    const data = await response.json()
+    newsList = data.articles
+
+    render()
+
 }
+
+const render = () => {
+    const newsHTML = newsList.map(
+        (news) => `<div class="row news">
+    <div class="col-lg-4">
+        <img class="news-img-size" src=${news.urlToImage} />
+    </div>
+    <div class="col-lg-8">
+        <h2>${news.title}</h2>
+        <p>${news.description}</p>
+        <div>
+            ${news.source.name} * ${news.publishedAt}
+        </div>
+    </div>
+</div>`).join('');
+    console.log("html", newsHTML)
+    document.getElementById('news-board').innerHTML = newsHTML;
+    
+}
+
+getLatestNews();
